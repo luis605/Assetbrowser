9d4e23350726a58e5225d160f49f614adf866855
TillWege <tillwege@gmail.com>
2024-10-15 20:02:07 +0200

    Add rlimgui and imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "imgui.h"
+#include "rlImGui.h"
 
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
@@ -9,10 +11,12 @@ int main(void)
 {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
 	SetTargetFPS(60);
+	rlImGuiSetup(true);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+		rlImGuiBegin();
 
 		ClearBackground(RAYWHITE);
 
@@ -20,9 +24,13 @@ int main(void)
 		const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
 		DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, SCREEN_HEIGHT / 2, 20, BLACK);
 
+		bool open = true;
+		ImGui::ShowDemoWindow(&open);
+
+		rlImGuiEnd();
 		EndDrawing();
 	}
-
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
